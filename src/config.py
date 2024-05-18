d99c16b50a6c04b778ea2945e19f72efab4613f2
midhilesh <chintu.midhilesh@gmail.com>
2024-05-19 00:23:22 +0530

    Added code for model pipeline

--- src/config.py
+++ src/config.py
@@ -5,15 +5,15 @@ from dotenv import load_dotenv
 script_dir = os.getcwd()  # Get the current working directory
 sys.path.append(os.path.abspath(os.path.join(script_dir, '..'))
                 )
-# from src.feature_store_api import FeatureGroupConfig, FeatureViewConfig
+from src.feature_store_api import FeatureGroupConfig, FeatureViewConfig
 from src.paths import PARENT_DIR
 
 # load key-value pairs from .env file located in the parent directory
-load_dotenv(PARENT_DIR / '.env')
+load_dotenv('.env')
 
-HOPSWORKS_PROJECT_NAME = 'taxi_demand'
+# HOPSWORKS_PROJECT_NAME = 'taxidemand_predictor'
 try:
-    # HOPSWORKS_PROJECT_NAME = os.environ['HOPSWORKS_PROJECT_NAME']
+    HOPSWORKS_PROJECT_NAME = os.environ['HOPSWORKS_PROJECT_NAME']
     HOPSWORKS_API_KEY = os.environ['HOPSWORKS_API_KEY']
 except:
     raise Exception(
@@ -23,53 +23,53 @@ except:
 # TODO: remove FEATURE_GROUP_NAME and FEATURE_GROUP_VERSION, and use FEATURE_GROUP_METADATA instead
 FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
 FEATURE_GROUP_VERSION = 1
-# FEATURE_GROUP_METADATA = FeatureGroupConfig(
-#     name='time_series_hourly_feature_group',
-#     version=3,
-#     description='Feature group with hourly time-series data of historical taxi rides',
-#     primary_key=['pickup_location_id', 'pickup_ts'],
-#     event_time='pickup_ts',
-#     online_enabled=True,
-# )
+FEATURE_GROUP_METADATA = FeatureGroupConfig(
+    name='time_series_hourly_feature_group',
+    version=3,
+    description='Feature group with hourly time-series data of historical taxi rides',
+    primary_key=['pickup_location_id', 'pickup_ts'],
+    event_time='pickup_ts',
+    online_enabled=True,
+)
 
 # # TODO: remove FEATURE_VIEW_NAME and FEATURE_VIEW_VERSION, and use FEATURE_VIEW_METADATA instead
-# FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
-# FEATURE_VIEW_VERSION = 4
-# FEATURE_VIEW_METADATA = FeatureViewConfig(
-#     name='time_series_hourly_feature_view',
-#     version=4,
-#     feature_group=FEATURE_GROUP_METADATA,
-# )
+FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
+FEATURE_VIEW_VERSION = 4
+FEATURE_VIEW_METADATA = FeatureViewConfig(
+    name='time_series_hourly_feature_view',
+    version=4,
+    feature_group=FEATURE_GROUP_METADATA,
+)
 
-# MODEL_NAME = 'taxi_demand_predictor'
+MODEL_NAME = 'taxidemand_predictor'
 
-# # added for monitoring purposes
-# # TODO remove FEATURE_GROUP_MODEL_PREDICTIONS and use FEATURE_GROUP_PREDICTIONS_METADATA instead
-# FEATURE_GROUP_MODEL_PREDICTIONS = 'model_predictions_feature_group'
-# FEATURE_GROUP_PREDICTIONS_METADATA = FeatureGroupConfig(
-#     name='model_predictions_feature_group',
-#     version=4,
-#     description='Predictions generate by our production model',
-#     primary_key=['pickup_location_id', 'pickup_ts'],
-#     event_time='pickup_ts',
-# )
+# added for monitoring purposes
+# TODO remove FEATURE_GROUP_MODEL_PREDICTIONS and use FEATURE_GROUP_PREDICTIONS_METADATA instead
+FEATURE_GROUP_MODEL_PREDICTIONS = 'model_predictions_feature_group'
+FEATURE_GROUP_PREDICTIONS_METADATA = FeatureGroupConfig(
+    name='model_predictions_feature_group',
+    version=4,
+    description='Predictions generate by our production model',
+    primary_key=['pickup_location_id', 'pickup_ts'],
+    event_time='pickup_ts',
+)
 
-# # TODO remove FEATURE_VIEW_MODEL_PREDICTIONS and use FEATURE_VIEW_PREDICTIONS_METADATA instead
-# FEATURE_VIEW_MODEL_PREDICTIONS = 'model_predictions_feature_view'
-# FEATURE_VIEW_PREDICTIONS_METADATA = FeatureViewConfig(
-#     name='model_predictions_feature_view',
-#     version=4,
-#     feature_group=FEATURE_GROUP_PREDICTIONS_METADATA,
-# )
+# TODO remove FEATURE_VIEW_MODEL_PREDICTIONS and use FEATURE_VIEW_PREDICTIONS_METADATA instead
+FEATURE_VIEW_MODEL_PREDICTIONS = 'model_predictions_feature_view'
+FEATURE_VIEW_PREDICTIONS_METADATA = FeatureViewConfig(
+    name='model_predictions_feature_view',
+    version=4,
+    feature_group=FEATURE_GROUP_PREDICTIONS_METADATA,
+)
 
-# MONITORING_FV_NAME = 'monitoring_feature_view'
-# MONITORING_FV_VERSION = 5
+MONITORING_FV_NAME = 'monitoring_feature_view'
+MONITORING_FV_VERSION = 5
 
-# # number of historical values our model needs to generate predictions
-# N_FEATURES = 24 * 28
+# number of historical values our model needs to generate predictions
+N_FEATURES = 24 * 28
 
-# # number of iterations we want Optuna to pefrom to find the best hyperparameters
-# N_HYPERPARAMETER_SEARCH_TRIALS = 1
+# number of iterations we want Optuna to pefrom to find the best hyperparameters
+N_HYPERPARAMETER_SEARCH_TRIALS = 1
 
-# # maximum Mean Absolute Error we allow our production model to have
-# MAX_MAE = 30.0
+# maximum Mean Absolute Error we allow our production model to have
+MAX_MAE = 30.0
